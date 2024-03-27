d4beb3dc98f9371a5fbf852d9942d7c65234ef2d
Stefan Heiden <stefanheiden@gmail.com>
2024-03-27 21:59:26 +0100

    add logging utility

--- cmd/engine.go
+++ cmd/engine.go
@@ -2,42 +2,101 @@ package commands
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"sync"
 	"time"
 )
 
 type Engine struct {
-	KeyChannel chan string
+	KeyChannel  chan string
+	DoneChannel chan struct{}
 }
 
 func (e Engine) Setup() {
+	Log.DebugLog.Printf("Setup Engine \n")
 	setupShellSettings()
 	e.KeyChannel = make(chan string, 1)
+	e.DoneChannel = make(chan struct{})
 }
 
 func (e Engine) StartSession() (string, error) {
+	Log.DebugLog.Printf("Start Session\n")
 	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go runTimer(3*time.Second, "Session 1", wg)
+	wg.Add(3)
+	go e.runTimer(10*time.Second, "Session 1", wg)
+	go e.inputKeyReceiver(wg)
+	go e.inputKeyHandler(wg)
 	wg.Wait()
+	close(e.KeyChannel)
+	close(e.DoneChannel)
 	return "Finished", nil
 }
 
-func runTimer(duration time.Duration, description string, wg *sync.WaitGroup) {
+func (e Engine) runTimer(duration time.Duration, description string, wg *sync.WaitGroup) {
+	Log.DebugLog.Printf("runTimer \n")
 	defer wg.Done()
 	startTime := time.Now()
 	for {
 		select {
-		default:
-			elapsed := time.Since(startTime)
-			fmt.Printf("Elapsed: %s", fmtDuration(elapsed))
-			time.Sleep(time.Second)
-			clearScreen()
-			if elapsed >= duration {
-				fmt.Println("Timer Stopped")
+		case key, ok := <-e.KeyChannel:
+			if !ok {
+				Log.DebugLog.Printf("keyChannel !ok\n")
 				return
 			}
+			switch key {
+			case "b":
+				Log.DebugLog.Printf("pressed b\n")
+				fmt.Printf("Break")
+			case "q":
+				Log.DebugLog.Printf("pressed q\n")
+				close(e.DoneChannel)
+				close(e.KeyChannel)
+			}
+		default:
+			elapsed := time.Since(startTime)
+			Log.Stdout.Printf("Elapsed: %s", fmtDuration(elapsed))
+			time.Sleep(time.Second)
+			if elapsed >= duration {
+				Log.Stdout.Println("Timer Stopped")
+				return
+			}
+		}
+	}
+}
+
+func (e *Engine) inputKeyReceiver(wg *sync.WaitGroup) {
+	Log.DebugLog.Printf("inputKeyReceiver\n")
+	defer wg.Done()
+	var b []byte = make([]byte, 1)
+	for {
+
+		select {
+		case _, ok := <-e.DoneChannel:
+			if !ok {
+				Log.DebugLog.Printf("Key Receiver done")
+				return
+			}
+		default:
+			Log.Stdout.Printf("Key please:")
+			//take care, this will block until key pressed
+			os.Stdin.Read(b)
+			i := string(b)
+			e.KeyChannel <- i
+			Log.Stdout.Printf("got key %s", i)
+		}
+	}
+}
+
+func (e *Engine) inputKeyHandler(wg *sync.WaitGroup) {
+	Log.DebugLog.Printf("inputKeyHandler\n")
+	defer wg.Done()
+	for {
+		select {
+
+		case key := <-e.KeyChannel:
+			Log.DebugLog.Printf("%s pressed", key)
+		default:
 		}
 	}
 }
---
+++ cmd/logging.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"log"
+	"os"
+)
+
+func init() {
+	Log = *NewLogging()
+}
+
+var Log Logging
+
+type Logging struct {
+	Stdout   *log.Logger
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+	DebugLog *log.Logger
+}
+
+func openLogFile(path string) *os.File {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	return f
+}
+
+func NewLogging() *Logging {
+	stdout := log.New(os.Stdout, "", 0)
+	infoLog := log.New(openLogFile("info.log"), "INFO\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	debugLog := log.New(openLogFile("debug.log"), "DEBUG\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &Logging{
+		Stdout:   stdout,
+		ErrorLog: errorLog,
+		InfoLog:  infoLog,
+		DebugLog: debugLog,
+	}
+
+	return app
+}
